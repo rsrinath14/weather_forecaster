d1b8bf9be28a2e7ce0efd32d6781aefc44234e17
R_Srinath <srinath1412001@gmail.com>
2020-07-21 06:50:33 +0530

    Connected API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(WeatherApp());
 
@@ -9,7 +11,40 @@ class WeatherApp extends StatefulWidget {
 
 class _WeatherAppState extends State<WeatherApp> {
   int temperature = 0;
-  String location = "Bengaluru";
+  int woeid = 2487956;
+  String location = 'San Francisco';
+  String weather = 'clear';
+  String searchApiUrl =
+      "https://www.metaweather.com/api/location/search/?query=";
+  String locationApiUrl = "https://www.metaweather.com/api/location/";
+
+  void fetchSearch(String input) async {
+    var searchResult = await http.get(searchApiUrl + input);
+    var result = json.decode(searchResult.body)[0];
+
+    setState(() {
+      location = result["title"];
+      woeid = result["woeid"];
+    });
+  }
+
+  void fetchLocation() async {
+    var locationResult = await http.get(locationApiUrl + woeid.toString());
+    var result = json.decode(locationResult.body);
+    var consolidatedWeather = result["consolidated_weather"];
+    var data = consolidatedWeather[0];
+
+    setState(() {
+      temperature = data["the_temp"].round();
+      weather = data["weather_state_name"].replaceAll(' ', '').toLowerCase();
+    });
+  }
+
+  void onTextFieldSubmitted(String input) {
+    fetchSearch(input);
+    fetchLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +52,7 @@ class _WeatherAppState extends State<WeatherApp> {
       home: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/clear.png'),
+            image: AssetImage('assets/$weather.png'),
             fit: BoxFit.cover,
           ),
         ),
@@ -48,7 +83,10 @@ class _WeatherAppState extends State<WeatherApp> {
                   Container(
                     width: 300,
                     child: TextField(
-                      style: TextStyle(color: Colors.white, fontSize: 30),
+                      onSubmitted: (String input) {
+                        onTextFieldSubmitted(input);
+                      },
+                      style: TextStyle(color: Colors.white, fontSize: 28),
                       decoration: InputDecoration(
                         hintText: "Search another location...",
                         hintStyle: TextStyle(color: Colors.white, fontSize: 20),
